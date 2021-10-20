8e7aa54d1d1bcd1e6f676e9804baa2d14d49250d
tung9898 <19103871d@connect.polyu.hk>
2021-10-21 00:08:48 +0800

    added function.

--- Monopoly.java
+++ Monopoly.java
@@ -1,9 +1,30 @@
 public class Monopoly{
+    /**
+     * Declare the borad, using a list of object.
+     */
     public static void main(String[] args) {
         /**
          * init board using list, make a list of square.
          * from 0 to 19
          * request a number from 2 to 6, use for the number of player.
          */
+    }
+
+    public void bankruptcy(int id){
+        /**
+         * This function will done all the things after a player has a
+         * negative amount of money.
+         * Set all of his/her properties to unowned.
+         */
+    }
+
+    public int[] rollingDice(){
+        /**
+         * This function will randomly generate 2 number in a specific range.
+         */
+        int[] r = new int[2];
+        r[0] = 0;
+        r[1] = 0;
+        return r;
     }
 }
