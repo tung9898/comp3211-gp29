3985c892428b1756085f71f20b49dde7c542f04d
tung9898 <19103871d@connect.polyu.hk>
2021-10-20 23:36:25 +0800

    Added a small description for main function.

--- Monopoly.java
+++ Monopoly.java
@@ -1,5 +1,9 @@
 public class Monopoly{
     public static void main(String[] args) {
-        
+        /**
+         * init board using list, make a list of square.
+         * from 0 to 19
+         * request a number from 2 to 6, use for the number of player.
+         */
     }
 }
