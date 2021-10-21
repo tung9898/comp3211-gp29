55730149b41f5d086707f853189e93f91d65c229
tung9898 <hoyiutung@hotmail.com>
2021-10-21 15:04:18 +0800

    Add GameStart() function and dice random.

--- Monopoly.java
+++ Monopoly.java
@@ -1,19 +1,40 @@
+import java.util.Random;
+
 public class Monopoly{
     /**
      * Declare the borad, using a list of object.
      */
-    Square[] board = new Square[20];
-    Player[] players;
+    static Random rand = new Random();
+    static Square[] board = new Square[20];
+    static Player[] players;
+    static int CurrentPlayer;
     public static void main(String[] args) {
+        
         /**
          * This funcion is the main function of Monopoly.
          */
         // init board using list, make a list of square, from 0 to 19
         // request a number from 2 to 6, use for the number of player.
         // Ask for number of players input.
+        
+        // start the game
+    }
 
-        // loop
-        // press enter to roll the dice.
+    public void GameStart(){
+        int turns = GameStatus.CurrentNumberOfPlayers;
+
+        for(int i = 0; i < turns; i++){
+            if(players[i].bankruptcy) continue;
+            CurrentPlayer = i;
+            if(players[i].DaysInJail > 0) {
+                // Check the player in jail days, if it is not -1, thats mean the player is in jail.
+            }
+            int[] dice = rollingDice();
+            PlayerMakeAMove(dice[0] + dice[1]);
+            // turns--;
+        }
+            
+        GameStatus.RoundEnd();
     }
 
     public void InitSqaure(int numberOfPlayer) {
@@ -25,7 +46,6 @@ public class Monopoly{
             board[i].Landlord = -1;
             board[i].PriceTag = SquareEffect.SquarePrice(i);
             board[i].Rent = SquareEffect.SquareRent(i);
-            board[i].InitplayerInSquare();
         }
         for(int i = 0; i < numberOfPlayer; i++) {
             players[i].Id = i;
@@ -41,6 +61,9 @@ public class Monopoly{
          * negative amount of money.
          * Set all of his/her properties to unowned.
          */
+        for(int i = 0; i < board.length; i++){
+            if(board[i].Landlord == id) board[i].Landlord = -1;
+        }
     }
 
     public int[] rollingDice() {
@@ -48,10 +71,9 @@ public class Monopoly{
          * This function will randomly generate 2 number in a specific range.
          */
         int[] r = new int[2];
-        r[0] = 0;
-        r[1] = 0;
-        //PlayerMakeAMove();
-        return r; // maybe no need to return
+        r[0] = rand.nextInt(4);
+        r[1] = rand.nextInt(4);
+        return r;
     }
 
     public void PlayerMakeAMove(int move) {
@@ -60,10 +82,11 @@ public class Monopoly{
          */
         // check if player is in jail
         // return if jail is not break
-        // int old_pos = current player pos;
-        // int new_pos = old_pos + move;
-        // current player pos = new_pos; // delete old pos and add new pos in Square.java
-        // new_pos sqaure effect
+        // int oldpos = current player pos;
+        // int newpos = oldpos + move;
+        // if newpos > 19, newpos -= 20;
+        // current player pos = newpos; // delete old pos and add new pos in Square.java
+        // newpos sqaure effect
 
         // Finally, Check player money. If less than 0, declare bankruptcies.
     }
