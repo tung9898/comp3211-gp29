056e37c82a516a874be5b4111dd1c421d74b33c4
tung9898 <hoyiutung@hotmail.com>
2021-10-22 11:14:39 +0800

    updated view

--- UserInterface.java
+++ UserInterface.java
@@ -1,33 +1,132 @@
+import java.util.List;
+
 public class UserInterface{
-    int OptionID;
-    String OptionName;
-    String Type;
-
-    public void setOptionID(int optionID) {
-        OptionID = optionID;
+    String nl = "\n";
+    String tab = "\t";
+    String space = " ";
+    String colon = ":";
+    String to = "-";
+    
+    public String PrintWelcomeMessage(){
+        return "Welcome to the Monopoly game!!";
     }
 
-    public int getOptionID() {
-        return OptionID;
+    public String PrintNumberOfPlayerInputMessage(int min, int max){
+        String msg = "Please enter the number of players from range of ";
+        msg += min + to + max + colon;
+        return msg;
     }
 
-    public void setOptionName(String optionName) {
-        OptionName = optionName;
+    public String PrintTurnStartedMessage(int pid){
+        return "It is player" + pid + "turn" + nl;
     }
 
-    public String getOptionName() {
-        return OptionName;
+    public String PrintTurnEndedMessage(){
+        return "Turn end" + nl;
     }
 
-    public void setType(String type) {
-        Type = type;
+    public String PrintRoundStartedMessage(){
+        return "Round " + GameStatus.rounds + "Start" + nl;
     }
 
-    public String getType() {
-        return Type;
+    public String PrintRoundEndedMessage(){
+        return "Round End" + nl;
     }
 
-    public void runOption() {
-        
+    public String PrintRoundOver100Message(){
+        return "Game Over, round is over 100." + nl;
     }
+
+    public String PrintWinnerMessage(List<Integer> w){
+        String msg;
+        if(w.size() == 1){
+            msg = "There is only one player left and the player is ";
+            msg += w.get(0);
+        }else{
+            msg = "There is " + w.size() + "left in the board, they are ";
+            for(int i = 0; i < w.size(); i++){
+                msg += w.get(i);
+            }
+        }
+        return msg;
+    }
+
+    public String PrintRequestRollDiceMessage(){
+        return "Press enter to poll the dice: ";
+    }
+
+    public String PrintRollDiceResultMessage(int[] d){
+        String msg = "";
+        for(int i = 0; i < d.length; i++){
+            msg += "Dice " + (i+1) + " : " + d[i] + nl;
+        }
+        return msg;
+    }
+
+    public String PrintCurrentPositionMessage(int pid){
+        String msg = "Player " + pid;
+        int pos = Monopoly.players[pid].CurrentSquare;
+        msg += ", your position is in board " + pos + nl;
+        if(SquareEffect.SquareName(pid) != "-1"){
+            msg += "Square Name: " + SquareEffect.SquareName(pid) + nl;
+            msg += "Square Price: " + SquareEffect.SquarePrice(pid) + nl;
+            msg += "Square Rent: " + SquareEffect.SquareRent(pid) + nl;
+        }
+        return msg;
+    }
+
+    public String PrintInJailDaysMessage(int pid){
+        return "";
+    }
+
+    public String PrintSquarePurchaseMessage(int pid){
+        return "";
+    }
+
+    public String PrintSquarePurchaseConfirmMessage(int pid){
+        return "";
+    }
+
+    public String PrintSquarePurchaseSuccessfulMessage(int pid){
+        return "";
+    }
+
+    public String PrintSquarePurchaseFailedMessage(int pid){
+        return "";
+    }
+
+    public String PrintSquarePayRentMessage(int pid){
+        return "";
+    }
+
+    public String PrintBankruptcyWarningMessage(int pid){
+        return "";
+    }
+
+    public String PrintBankruptcyMessage(int pid, int[] pprop){
+        return "";
+    }
+
+    // Square Effect
+    public String PrintPassGOMessage(){
+        return "";
+    }
+
+    public String PrintChanceMessage(){
+        return "";
+    }
+
+    public String PrintPayTaxMessage(){
+        return "";
+    }
+
+    public String PrintNoEffectMessage(int sid){
+        return "";
+    }
+
+    public String PrintSquareInfoMessage(int sid){
+        return "";
+    }
+
+    
 }
