77c3b7a0f349f2d04d83a669342dd747a838bfdd
tinwai0708@gmail.com <tinley.y78@gmail.com>
2021-11-02 01:00:08 +0800

    MoneyController -> PlayerController

--- src/main/Controller/PlayerController.java
+++ src/main/Controller/PlayerController.java
@@ -25,4 +25,30 @@ public class PlayerController {
     public void retirePlayer(){
         
     }
+
+    public void addMoney(int income){
+        /**
+         * This function will add the player's money by the income.
+         */
+        int money = model.getMoney();
+        money += income;
+        model.setMoney(money);
+    }
+
+    public void reduceMoney(int outcome){
+        /**
+         * This function will subtract the player's money by the income.
+         */
+        int money = model.getMoney();
+        money -= outcome;
+        model.setMoney(money);
+    }
+
+    public void payTax(){
+        //
+    }
+
+    public void receiveTax(){
+        //
+    }
 }
