1af3afc69a25488e4c804198f410e5bc229fd53a
tinwai0708@gmail.com <tinley.y78@gmail.com>
2021-11-18 15:31:57 +0800

    del two tab of leaderboard

--- src/main/View/PlayerView.java
+++ src/main/View/PlayerView.java
@@ -123,7 +123,7 @@ public class PlayerView extends UserInterface{
             // leaderboard[i] is the i rank in the LeaderBoard
             // leaderboard[i][0] is the rank number, they will be the same if they own the same balance.
             // leaderboard[i][1] is the player ID and leaderboard[i][2] is the player balance.
-            msg += leaderboard[i][0] + tab + tab + (leaderboard[i][1] + 1) + tab + tab + tab + "$" + leaderboard[i][2] + nl;
+            msg += leaderboard[i][0] + tab + tab + (leaderboard[i][1] + 1) + tab + "$" + leaderboard[i][2] + nl;
         }
         msg += "==============================================\n";
         return msg;
